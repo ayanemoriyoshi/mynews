c5bbe9b4fb7a096483a3aa5a352d2c002b507b1f
EC2 Default User <ec2-user@ip-172-31-2-158.us-east-2.compute.internal>
2021-10-19 08:43:16 +0000

    first commit

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //1つ目
+    public function add()
+{
+    return view('admin. profile. create');
+}
+
+{
+    //2つ目
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+
+{
+    //3つ目
+    public function edit()
+    {
+        return view('admin.profile.create');
+    }
+}
+
+{
+    //4つ目
+    public function uptate()
+    {
+        return redirect('admin/profile/create');
+    }
+}
+}
