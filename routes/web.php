491bcb09d989470b08c8a8799894293f292468e2
EC2 Default User <ec2-user@ip-172-31-2-158.us-east-2.compute.internal>
2021-10-21 03:18:07 +0000

    routes/web.phpにRoutingの設定を追加 2回目 PHP/Laravel 04の課題提出 211021

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,10 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-});
 
-Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-});
 
-Route::group(['prefix' => 'admin'], function() {
     Route::get('admin/profile/create', 'Admin\ProfileController@add');
-});
 
-Route::group(['prefix' => 'admin'], function() {
     Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
 });
