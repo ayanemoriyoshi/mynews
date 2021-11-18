029286bcd195f02a341f51e962d724b30300a183
moriyoshi <pyon87.sugar@gmail.com>
2021-11-18 12:55:02 +0000

    PHP/Laravel 16 が終了

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -9,42 +9,52 @@ use App\Profile;
 class ProfileController extends Controller
 {
 
-        //1つ目
         public function add()
         {
         return view('admin.profile.create');
-        
-            
         }
        
-    
-    
-        //3つ目
-        public function edit()
+       
+        
+        public function edit(Request $request)
         {
-            return view('admin.profile.create');
+          
+          $profiles = Profile::find($request->id);
+          if (empty($profiles)) {
+            abort(404);    
+          }
+          return view('admin.profile.edit', ['profiles_form' => $profiles]);
         }
-    
-    
-    
-        //4つ目
-        public function uptate()
+        
+        
+        public function uptate(Request $request)
         {
+              // Validationをかける
+            $this->validate($request, Profile::$rules);
+              // News Modelからデータを取得する
+            $profiles = Profile::find($request->id);
+              // 送信されてきたフォームデータを格納する
+            $profiles_form = $request->all();
+            unset($profiles_form['_token']);
+        
+              // 該当するデータを上書きして保存する
+            $profiles->fill($profiles_form)->save();
             return redirect('admin/profile/create');
         }
     
-     public function create(Request $request)
-  {
-    // 以下を追記
-      // Varidationを行う
-      $this->validate($request, Profile::$rules);
-      $profiles = new Profile;
-      $form = $request->all();
-      
-      $profiles->fill($form);
-      $profiles->save();
-      
-      // admin/news/createにリダイレクトする
-      return redirect('admin/profile/create');
-  }  
+        public function create(Request $request)
+        {
+              // Varidationを行う
+              $this->validate($request, Profile::$rules);
+              $profiles = new Profile;
+              $form = $request->all();
+              
+              $profiles->fill($form);
+              $profiles->save();
+              
+              // admin/news/createにリダイレクトする
+              return redirect('admin/profile/create');
+        }  
+  
+  
 }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,18 @@ Route::group(['prefix' => 'admin'], function() {
     
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     Route::post('profile/edit', 'Admin\ProfileController@update');
+    
+    
 });
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+});
